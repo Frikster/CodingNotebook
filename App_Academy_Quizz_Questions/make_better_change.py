8c060303d5a2fe0641f8542cb50bc4e32c9edd6f
cornelis <dirk.haupt@gmail.com>
2019-01-10 13:20:02 -0800

    solve self-made make_change problem

--- App_Academy_Quizz_Questions/make_better_change.py
+++ App_Academy_Quizz_Questions/make_better_change.py
@@ -10,21 +10,40 @@
 # of each denomination.
 
 # simply modify as below commented out to make this work for returning the one with the least amount of coins
+def make_better_change_better(value, idx=0, coins=[1,5,10,25], memo={}):
+    if (value,idx) in memo: return memo[(value, idx)]
+    if value <= 0 or idx>=len(coins): return [[]]
+    # coins = list(filter(lambda c: c <= value, coins))
+    res = []
+    coin = coins[idx] 
+    branches = value // coin  # max number of times we can pick this coin.
+    while branches >= 0:
+        if coin <= value:
+            combos_without_coin = make_better_change_better(value-branches*coin, idx+1, coins, memo)
+            for combo in combos_without_coin:
+                res.append(branches*[coin] + combo)
+        branches -= 1
+    memo[(value, idx)] = res
+    return res
+
+# less optimized than the one above. Computes all possible subsets but only returns one valid coin combo for each due to the use of sets
 def make_better_change(value, coins, memo={}):
-    if value <= 0: return [[]]
-    res = [] 
-    for idx, coin in enumerate(coins): 
+    if value <= 0: return [tuple()]
+    coins = set(filter(lambda c: c <= value, coins))
+    res = set() 
+    for idx, coin in enumerate(coins):
         if coin <= value:
             if (value - coin, idx) in memo: 
                 combos_without_coin = memo[(value - coin, idx)]
             else:
                 combos_without_coin = make_better_change(value - coin, coins, memo)
+                combos_without_coin = {tuple(sorted(combo)) for combo in combos_without_coin}
                 memo[(value - coin, idx)] = combos_without_coin
             for combo in combos_without_coin:
-                res.append([coin] + combo)
+                res.add(tuple(sorted((coin,) + combo)))
     return res
 
-
+# Only returns number (can be further optimized with memo)
 def coin_representations(n, denom_index=0, denoms=(1, 5, 10, 25)):
     if n == 0:  # base case: we've found a combination of coins that divides evenly into amount of change
         return 1
@@ -40,36 +59,38 @@ def coin_representations(n, denom_index=0, denoms=(1, 5, 10, 25)):
         branches -= 1
     return representations
 
-# def make_better_change(value, coins):
-#     coins_to_check = list(filter(lambda coin: coin <= value, coins))
-#     if not coins_to_check: return []
+# Least optimized. Returns all possible subsets
+def make_change(value, coins):
+    coins_to_check = list(filter(lambda coin: coin <= value, coins))
+    if not coins_to_check: return []
+
+    solutions = []
+
+    coins_to_check.sort(reverse=True)
+    for coin in coins_to_check:
+        remainder = value - coin
+
+        if remainder > 0:
+            # remainder_solution = make_change(remainder, coins_to_check) # use commented out to only return one with least amount of coins
+            # if remainder_solution:
+            #     solutions.append([coin] + remainder_solution) 
+
+            remainder_solutions = make_change(remainder, coins_to_check)
+            if remainder_solutions:
+                for remainder_solution in remainder_solutions:
+                    solutions.append([coin] + remainder_solution)
+
+        else:
+            solutions.append([coin])
     
-
-#     solutions = []
-
-#     coins_to_check.sort(reverse=True)
-#     for coin in coins_to_check:
-#         remainder = value - coin
-
-#         if remainder > 0:
-#             # remainder_solution = make_better_change(remainder, coins_to_check) # use commented out to only return one with least amount of coins
-#             # if remainder_solution:
-#             #     solutions.append([coin] + remainder_solution) 
-
-#             remainder_solutions = make_better_change(remainder, coins_to_check)
-#             if remainder_solutions:
-#                 for remainder_solution in remainder_solutions:
-#                     solutions.append([coin] + remainder_solution)
-
-#         else:
-#             solutions.append([coin])
-    
-#     return sorted(solutions, key=lambda arr: len(arr))
+    return sorted(solutions, key=lambda arr: len(arr))
     # return sorted(solutions, key=lambda arr: len(arr))[0]
 
 
 print(len(make_better_change(24, [1, 5, 10, 25])))
+print((make_better_change(24, [1, 5, 10, 25])))
 print(coin_representations(24))
+print(make_better_change_better(24))
 
 # print(make_better_change(24, [10, 7, 1]))
 # print(len(make_better_change(24, [10, 7, 1])))
