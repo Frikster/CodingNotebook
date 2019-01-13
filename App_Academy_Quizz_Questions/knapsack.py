234c304c5320f215c0a77bc026ed6974abe6f641
cornelis <dirk.haupt@gmail.com>
2019-01-13 15:44:39 -0800

    finish and optimize knapsack top-down and bottom-up

--- App_Academy_Quizz_Questions/knapsack.py
+++ App_Academy_Quizz_Questions/knapsack.py
@@ -8,6 +8,7 @@
 import pdb
 # from functools import lru_cache
 
+# Top-down - gives the weight-value pairs chosen. Simply iterate through to get total value
 class Knapsack:
     def __init__(self):
         self.cache = {}
@@ -21,95 +22,59 @@ class Knapsack:
         if weight > capacity:
             return self.knapsack(weights_values, capacity, items_left-1)
         else:
-            with_item = self.knapsack(weights_values, capacity-weight, items_left-1)
+            with_item = [(weight, value)] + self.knapsack(weights_values, capacity-weight, items_left-1)
             without_item = self.knapsack(weights_values, capacity, items_left-1)
-            # pdb.set_trace()
-            with_item_val = weight + sum([item[1] for item in with_item]) if with_item else 0
+            with_item_val = value + sum([item[1] for item in with_item]) if with_item else 0
             without_item_val = sum([item[1] for item in without_item]) if without_item else 0
             if with_item_val >= without_item_val:
-                res += [(weight, value)] + with_item
+                res += with_item
             else: 
                 res += without_item
 
-
-
-
-            #     max_combo = max(combos_without_item, key=lambda combo: sum(
-            #         [c[1] for c in combo]))
-            #     max_val = sum([c[1] for c in max_combo])
-            #     combos_without_item = filter(lambda combo: sum(
-            #         [c[1] for c in combo]) == max_val, combos_without_item)
-            # for combo in combos_without_item:
-            #     res.append([(weight, value)] + combo)
         self.cache[(capacity, items_left)] = res
         return res
 
+# Bottom-up
+def knapsack(weight_values, capacity):
+    if capacity == 0 or len(weight_values) == 0 : return []
+    solution_table = knapsack_table(weight_values, capacity)
+    return solution_table[(capacity, len(weight_values)-1)]
 
-# https://codereview.stackexchange.com/questions/20569/dynamic-programming-knapsack-solution
-    # def knapsack_best(items, maxweight):
-    #     """Solve the knapsack problem by finding the most valuable subsequence
-    #     of items that weighs no more than maxweight.
+def knapsack_table(weight_values, capacity):
+    solution_table = {}
+    # Build solutions for knapsacks of increasing capacity
+    for cap in range(capacity+1):
+        for item in range(len(weight_values)):
+            weight, value = weight_values[item]
+            if cap <= 0:
+                solution_table[(cap, item)] = []
+            elif item == 0:
+                # for the first item in our list, you must check for capacity
+                # if there is, then you enter its value in the first slot, otherwise 0
+                solution_table[(cap, item)] = [(weight, value)] if weight <= cap else []
+            else:
+                # the first option is the entry from considering the previous item at this capacity
+                option1 = solution_table[(cap, item - 1)]
+                option1_val = sum([wv[1] for wv in option1]) if option1 else 0
+                # the second option (assuming enough capacity) is the entry from a smaller bag
+                # (with enough room for this item) plus this item's value
+                # other_items = solution_table[(cap - weight,item - 1)] if cap - weight >= 0 else []
+                option2 = [(weight, value)] + solution_table[(cap - weight, item - 1)] if cap - weight >= 0 else []
+                option2_val = value + sum([wv[1] for wv in option2]) if option2 else 0
 
-    #     items must be a sequence of pairs (value, weight), where value is a
-    #     number and weight is a non-negative integer.
+                # the actual entry for this item is the optimum of the two choices
+                optimum = option2 if option2_val >= option1_val else option1
+                solution_table[(cap, item)] = optimum
 
-    #     maxweight is a non-negative integer.
+    return solution_table
 
-    #     Return a pair whose first element is the sum of values in the most
-    #     valuable subsequence, and whose second element is the subsequence.
-
-    #     >>> items = [(4, 12), (2, 1), (6, 4), (1, 1), (2, 2)]
-    #     >>> knapsack(items, 15)
-    #     (11, [(2, 1), (6, 4), (1, 1), (2, 2)])
-
-    #     """
-    #     @lru_cache(maxsize=None)
-    #     def bestvalue(i, j):
-    #         # Return the value of the most valuable subsequence of the first
-    #         # i elements in items whose weights sum to no more than j.
-    #         if j < 0:
-    #             return float('-inf')
-    #         if i == 0:
-    #             return 0
-    #         value, weight = items[i - 1]
-    #         return max(bestvalue(i - 1, j), bestvalue(i - 1, j - weight) + value)
-
-    #     j = maxweight
-    #     result = []
-    #     for i in reversed(range(len(items))):
-    #         if bestvalue(i + 1, j) != bestvalue(i, j):
-    #             result.append(items[i])
-    #             j -= items[i][1]
-    #     result.reverse()
-    #     return bestvalue(len(items), maxweight), result
-
-
-
-
-# Top-down... this "solution" forgot that **duplicates are not allowed!!**
-# class Knapsack:
-#     def __init__(self):
-#         self.cache = {}
-
-#     def knapsack(self, weights_values, capacity, idx=0):
-#         if (capacity,idx) in self.cache: return self.cache[(capacity, idx)]
-#         if capacity <= 0 or idx >= len(weights_values): return [[]]
-#         weight, value = weights_values[idx]
-#         branches = capacity // weight # max number of times this weight can be used
-#         res = []
-#         while branches >= 0:
-#             if weight <= capacity:
-#                 combos_without_item = self.knapsack(weights_values, capacity-branches*weight, idx+1)
-#                 # TODO: fix this
-#                 if combos_without_item:
-#                     max_combo = max(combos_without_item, key=lambda combo: sum([c[1] for c in combo]))
-#                     max_val = sum([c[1] for c in max_combo])
-#                     combos_without_item = filter(lambda combo: sum([c[1] for c in combo]) == max_val, combos_without_item)
-#                 for combo in combos_without_item:
-#                     res.append([(weight, value)]*branches + combo)
-#             branches-=1
-#         self.cache[(capacity, idx)] = res
-#         return res
 
 print(Knapsack().knapsack([(1,10),(2,4),(3,10),(4,4)], 5))
+print(Knapsack().knapsack([(4, 4), (3, 10), (2, 4), (1, 10)], 5)) 
 print(Knapsack().knapsack([(2, 10), (3, 4), (3, 10), (4, 4)], 7))
+print(Knapsack().knapsack([(4, 4), (3, 10), (3, 4), (2, 10)], 7))
+
+print(knapsack([(1, 10), (2, 4), (3, 10), (4, 4)], 5))
+print(knapsack([(4, 4), (3, 10), (2, 4), (1, 10)], 5))
+print(knapsack([(2, 10), (3, 4), (3, 10), (4, 4)], 7))
+print(knapsack([(4, 4), (3, 10), (3, 4), (2, 10)], 7))
