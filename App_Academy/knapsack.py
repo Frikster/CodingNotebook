030821d9594b218e058a321e8c7f07fcb2bc21d0
cornelis <dirk.haupt@gmail.com>
2019-02-07 00:30:43 -0800

    ank knapsack

--- App_Academy/knapsack.py
+++ App_Academy/knapsack.py
@@ -1,12 +1,9 @@
-#   Knapsack Problem: write a function that takes in an array of weights, an array of values,
-#   and a weight capacity and returns the maximum value possible given the weight constraint.
-#   For example: if weights = [1, 2, 3], values = [10, 4, 8], and capacity = 3, your function 
-#   should return 10 + 4 = 14, as the best possible set of items to include are items 0 and 1, 
-#   whose values are 10 and 4 respectively. Duplicates are not allowed - - that is, you can 
-#   only include a particular item once.
-
-import pdb
-# from functools import lru_cache
+#   Knapsack Problem: write a function that takes in an array of weight-value tuples,
+#   and a weight capacity and returns the an array of weight-value tuples that maximizes
+#   their total value given the weight capacity constraint.
+#   For example: if weight-value = [(1,10),(2,4),(3,8)] and capacity = 3, your function
+#   should return [(1,10),(2,4)] as the best possible set of items
+#   **Duplicates are not allowed** - - that is, you can only include a particular item once.
 
 # Top-down - gives the weight-value pairs chosen. Simply iterate through returned values to get total value
 class Knapsack:
