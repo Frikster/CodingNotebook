6d82fe22d79cd6632957377f7c9c41350492f6d6
cornelis <dirk.haupt@gmail.com>
2019-03-01 23:20:20 -0800

    code cleanup

--- Leetcode/coin_change.py
+++ Leetcode/coin_change.py
@@ -12,25 +12,22 @@
 # Note:
 # You may assume that you have an infinite number of each kind of coin.
 
-# Forget your method. This is the path to enlightenment:
 # App Academy Burglar problem gives great insight into this
 def coinChange(self, coins, amount):
             if not amount:
                     return 0
 
-            dp = [float("inf") for i in range(amount+1)]
+            dp = [float("inf")] * (amount+1)
 
-            for i in range(1, amount+1):
-                if i in coins:
-                    dp[i] = 1
-                else:
-                    new = [coin for coin in coins if coin < i]
-                    if not new:
-                        dp[i] = float("inf")
-                    for j in new:
-                        # check each of the coins you could use to get to this amount
-                        dp[i] = min(dp[i], 1 + dp[i-j])
-            if dp[-1] == float("inf"):
-                return -1
-
+            for val in range(1, amount+1):
+                final_coins = [coin for coin in coins if coin <= val]
+                # check each of the coins you could use to get to this amount
+                for final_coin in final_coins:
+                    if final_coin == val:
+                        dp[val] = 1
+                        break
+                    else:
+                        dp[val] = min(dp[val], dp[val-final_coin]+1)
+            if dp[-1] == float('inf'):
+                    return -1
             return dp[-1]
